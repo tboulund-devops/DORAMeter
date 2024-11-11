e144e54dbf9b69c903a2475bfc8f3d874cb24964
tboulund <tbmh@easv.dk>
2024-11-11 08:15:34 +0100

    Created handler for hotfix branch created

---
+++ src/BLL/GitHubPayloadStrategies/HotfixBranchCreatedStrategy.cs
@@ -0,0 +1,9 @@
+namespace BLL.GitHubPayloadStrategies;
+
+public class HotfixBranchCreatedStrategy : IGitHubPayloadHandler
+{
+    public void Handle(Dictionary<string, object> payload)
+    {
+        throw new NotImplementedException();
+    }
+}
