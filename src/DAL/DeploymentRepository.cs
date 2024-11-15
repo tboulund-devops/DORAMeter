21fa501f4ddbf772831e1df6e4da74f21ba75f53
tboulund <tbmh@easv.dk>
2024-11-15 10:09:59 +0100

    Deployment repository returned wrong instance

--- src/DAL/DeploymentRepository.cs
+++ src/DAL/DeploymentRepository.cs
@@ -8,8 +8,10 @@ public class DeploymentRepository : RepositoryBase
     public Deployment RegisterDeployment(DateTime startDate, DateTime endDate)
     {
         using var connection = OpenConnection();
-        var deployment = new Deployment(startDate, endDate);
-        connection.ExecuteScalar<int>("INSERT INTO deployments (start_date, end_date) VALUES (@StartDate, @EndDate); SELECT LAST_INSERT_ID()", new { StartDate = startDate, EndDate = endDate });
-        return new Deployment(startDate, endDate);
+        var deployment = new Deployment(startDate, endDate)
+        {
+            Id = connection.ExecuteScalar<int>("INSERT INTO deployments (start_date, end_date) VALUES (@StartDate, @EndDate); SELECT LAST_INSERT_ID()", new { StartDate = startDate, EndDate = endDate })
+        };
+        return deployment;
     }
 }
